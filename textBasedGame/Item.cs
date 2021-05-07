5ffc36092e34c945a43be96121218b7b054f754e
donutgirl337 <littlechica12@gmail.com>
2021-05-07 13:19:00 -0400

    hi

--- textBasedGame/Item.cs
+++ textBasedGame/Item.cs
@@ -6,13 +6,47 @@ namespace textBasedGame
 {
     class Item
     {
+        private string _storage;
+        private bool _conditions;
 
-        public Item() 
-        {
+        Item name = new Item("name", true);
+        parentObject.addItem(name);
+
+        Item Mop = new Item("mop", true);
+                CleaningCloset.addItem(Mop);
+
+        Item Rag = new Item(“Rag", true); 
+        CleaningCloset.addItem(Rag);
+
+                Item ToiletPaper = new Item("Toilet Paper", true);
+                CleaningCloset.addItem(ToiletPaper);
+
+        Item Bucket = new Item("Bucket", true);
+                CleaningCloset.addItem(Bucket);
+
+        Item Napkins = new Item("Napkins", true);
+                FoodSupplies.addItem(Napkins);
+
             
+        Item ToiletPaper = new Item("Toilet Paper", true);
+                CleaningCloset.addItem(ToiletPaper);
+
+        Item Bucket = new Item("Bucket", true);
+                CleaningCloset.addItem(Bucket);
+
+        Item Napkins = new Item("Napkins", true);
+                FoodSupplies.addItem(Napkins);
+
+        Item Condiments = new Item("Condiments", true);
+        FoodSupplements.addItem(Condiments);
+
+
+        public Item(string Storage) 
+        {
+            _storage = Storage;
         }
 
-
+        
 
     }
 }
